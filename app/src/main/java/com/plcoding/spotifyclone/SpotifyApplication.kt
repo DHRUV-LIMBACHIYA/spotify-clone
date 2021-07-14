64812f7ae46e2b69d7d6f14bb637dd0368e27c58
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-07-14 14:56:41 +0530

    dagger-hilt setup

---
+++ app/src/main/java/com/plcoding/spotifyclone/SpotifyApplication.kt
@@ -0,0 +1,11 @@
+package com.plcoding.spotifyclone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Dhruv Limbachiya on 14-07-2021.
+ */
+
+@HiltAndroidApp
+class SpotifyApplication : Application()
